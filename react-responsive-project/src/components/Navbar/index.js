c806e92c3ff7831dd9aa093404fcadf46529c67b
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-30 01:01:03 -0700

    Navbar

---
+++ react-responsive-project/src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+// rfce / rafce (arror fn)
+import React from 'react';
+import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo>Jenny Park</NavLogo>
+      </NavbarContainer>
+    </Nav>
+  )
+}
+
+export default Navbar
